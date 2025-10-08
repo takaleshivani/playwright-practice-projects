5aca1b0deb2bd583cf3d59c236ca49ddddde2f12
Shivani Takale <takaleshivani10@gmail.com>
2025-10-08 09:13:47 +0530

    Completed level-1-Checkout & API challenge and added docblock using GitHUB Copilot

--- tests/challenge.spec.ts
+++ tests/challenge.spec.ts
@@ -1,5 +1,11 @@
 import {test, expect} from '@playwright/test';
 
+/**
+ * Checkout Challenge Test
+ * Simulates a logged-in customer adding a product to the cart,
+ * completing checkout steps, selecting a payment method,
+ * and verifying successful payment.
+ */
 test.describe('Checkout Challenge', () => {
     test.use({ storageState: "./.auth/customer01.json" });
 
@@ -7,8 +13,10 @@ test.describe('Checkout Challenge', () => {
         await page.goto('https://practicesoftwaretesting.com/');
     });
 
-  test('Add product to cart', async ({ page,headless }) => {
-    await page.locator('[data-test="product-01K70RRBBFWQF2M2Y8KQZTKQ3D"]').click();
+    
+
+    test('Add product to cart', async ({ page,headless }) => {
+    await page.getByTestId('product-01K70W668GCKKHDGB27FJ5VQX9').click();
     await page.locator('[data-test="add-to-cart"]').click();
     await page.locator('div').filter({ hasText: 'Product added to shopping' }).nth(2).click();
     await page.locator('[data-test="nav-cart"]').click();
@@ -32,4 +40,36 @@ test.describe('Checkout Challenge', () => {
   });
 
   
+});
+
+
+/**
+ * API Challenge Test
+ * Sends requests to the product API to search for a product by name,
+ * retrieves its details, and verifies the product's name, price, description, and ID.
+ */
+test.describe('API challenge', () => {
+    test('Get /products/{id}', async ({ request }) => {   
+        const apiURL = "https://api.practicesoftwaretesting.com";
+        const getProductResponse = await request.get(
+                apiURL + "/products/search?q=thor%20hammer"
+        );
+        expect(getProductResponse.status()).toBe(200);
+        const productBody = await getProductResponse.json();
+        const productId = productBody.data[0].id;
+
+        const response = await request.get(apiURL + "/products/" + productId);
+
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        console.log(responseBody);
+        console.log("Product Name : ", responseBody.name);
+        expect(responseBody.name).toBe("Thor Hammer");
+        expect(responseBody.price).toBe(11.14);
+        expect(responseBody.description).toBe(
+                "Donec malesuada tempus purus. Integer sit amet arcu magna. Sed vel laoreet ligula, non sollicitudin ex. Mauris euismod ac dolor venenatis lobortis. Aliquam iaculis at diam nec accumsan. Ut sodales sed elit et imperdiet. Maecenas vitae molestie mauris. Integer quis placerat libero, in finibus diam. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+        );
+        expect(responseBody.id).toBe(productId);
+    }
+    );
 });
